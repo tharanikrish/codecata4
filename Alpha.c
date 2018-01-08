8ca71bcd7b530f5677f0520e95d33b339b0e6725
tharanikrish <31505996+tharanikrish@users.noreply.github.com>
2018-01-08 19:23:44 +0530

    Update Alpha.c

--- Alpha.c
+++ Alpha.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-char a;
+char c;
 printf("Enter a character");
 scanf("%c",&c);
-if((c>=a && c<=z) || (c>=A &&c<=Z))
+if((c>='a' && c<='z') || (c>='A' &&c<='Z'))
 printf("/nThe character is alphabet");
 else
 printf("/nThe character is not an alphabet");
